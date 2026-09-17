762c4a212fdb5e12f0c1bd47d92d21aa9ab74fec
insaftjj <https://github.com/insaftjj/youcode-sas.git>
2026-09-17 12:41:16 +0200

    finalproject

---
+++ src/functions.js
@@ -0,0 +1,21 @@
+
+
+import { apprenants } from "./data.js"; 
+export function normaliserNom(nom) {
+      return nom.trim().toLowerCase();
+}
+export function
+validerResultatt(resultat){
+    return typeof resultat==='number' && resultat >= 0 && resultat <=20;
+}
+export function
+ajouterApprenant(nomcomplet,ville){
+    const nouvelApprenant = {
+        id: apprenants.length + 1,
+        nomComplet: normaliserNom(nomComplet),
+        ville: ville,
+        resultat:[]
+    };
+apprenants.push(nouvelApprenant);
+    return nouvelApprenant;
+}

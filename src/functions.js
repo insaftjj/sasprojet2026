1dadb5c9c48d6b5a4c7ace0cdf458002889fa519
insaftjj <https://github.com/insaftjj/youcode-sas.git>
2026-09-17 20:19:21 +0200

    j'ai finie quatre quatre fonction premiere

--- src/functions.js
+++ src/functions.js
@@ -1,21 +1,53 @@
-
-
 import { apprenants } from "./data.js"; 
 export function normaliserNom(nom) {
       return nom.trim().toLowerCase();
 }
-export function
-validerResultatt(resultat){
-    return typeof resultat==='number' && resultat >= 0 && resultat <=20;
+export function validerResultat(jour, exercicesTermines, totalExercices) {
+    if (jour < 1 || jour > 7) {
+        return false;
+    }
+
+    if (exercicesTermines > totalExercices) {
+        return false;
+    }
+
+    if ( exercicesTermines < 0 || totalExercices < 0){
+        return false; 
+    }
+
+    return true;
 }
-export function
-ajouterApprenant(nomcomplet,ville){
-    const nouvelApprenant = {
+ export function ajouterApprenant(nomComplet, ville) {
+    for (let i of apprenants) {
+        if (nomComplet === i.nomComplet) {
+            console.log("Cet apprenant existe déjà !");
+            return false;
+        }
+    }
+    const object = {
         id: apprenants.length + 1,
         nomComplet: normaliserNom(nomComplet),
         ville: ville,
-        resultat:[]
+        resultat: []
     };
-apprenants.push(nouvelApprenant);
-    return nouvelApprenant;
-}  
+
+    apprenants.push(object);
+    return true;
+}
+export function enregistrerResultat(id,resultat) {
+    for(let apprenant of apprenants) {
+        if(apprenant.id=== id)
+        {
+            for(let i=0;i<apprenant.resultats.length; i++)
+            {
+                if(apprenant.resultats[i].jour===resultat.jour){
+                    apprenant.resultats[i]=resultat;
+                    return;
+                }
+            }
+            apprenant.resultats.push(resultat);
+            return;
+        }
+    }
+}
+
---
+++ src/index.js
@@ -0,0 +1,19 @@
+import { apprenants } from "./data.js";
+import{
+ normaliserNom,
+ validerResultat,
+ ajouterApprenant,
+}from "./functions.js" 
+
+do{
+  console.log("1. Afficher le tableau de bord") 
+  console.log("2. Afficher la liste des apprenants")
+  console.log("3. Ajouter un apprenant")
+  console.log("4. Consulter un apprenant par identifiant")
+  console.log("5. Ajouter ou modifier le résultat d'une journée")
+  console.log("6. Rechercher un apprenant par nom")
+  console.log("7. Filtrer les apprenants par niveau")
+  console.log("8. Trier les apprenants par progression décroissante ")
+  console.log("0. Quitter")
+  
+} while( )

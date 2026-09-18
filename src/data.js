aa6b2a60567898b8e71337db2f495d4d4208a991
insaftjj <https://github.com/insaftjj/youcode-sas.git>
2026-09-18 17:25:41 +0200

    entrer nom et prenom

--- src/data.js
+++ src/data.js
@@ -1,22 +1,141 @@
 export const apprenants = [
- {
- id: 1,
- nomComplet: "Sara Dev",
- ville: "Nador",
- resultats: [
- { jour: 1, exercicesTermines: 18,
- totalExercices: 20, challengeTermine: true },
- { jour: 2, exercicesTermines: 14,
- totalExercices: 20, challengeTermine: false }
- ]
- },
- {
- id: 2,
- nomComplet: "Yassine Code",
- ville: "Oujda",
- resultats: [
- { jour: 1, exercicesTermines: 12,
- totalExercices: 20, challengeTermine: false }
- ]
- }
+  {
+    id: 1,
+    nomComplet: "Sara Dev",
+    ville: "Nador",
+    resultats: [
+      { jour: 1, exercicesTermines: 18, totalExercices: 20, challengeTermine: true },
+      { jour: 2, exercicesTermines: 14, totalExercices: 20, challengeTermine: false }
+    ]
+  },
+  {
+    id: 2,
+    nomComplet: "Yassine Code",
+    ville: "Oujda",
+    resultats: [
+      { jour: 1, exercicesTermines: 12, totalExercices: 20, challengeTermine: false }
+    ]
+  },
+  {
+    id: 3,
+    nomComplet: "Karim Web",
+    ville: "Berkane",
+    resultats: [
+      { jour: 1, exercicesTermines: 20, totalExercices: 20, challengeTermine: true },
+      { jour: 2, exercicesTermines: 19, totalExercices: 20, challengeTermine: true }
+    ]
+  },
+  {
+    id: 4,
+    nomComplet: "Amina Tech",
+    ville: "Al Hoceima",
+    resultats: [
+      { jour: 1, exercicesTermines: 5, totalExercices: 20, challengeTermine: false }
+    ]
+  },
+  {
+    id: 5,
+    nomComplet: "Omar Data",
+    ville: "Nador",
+    resultats: [
+      { jour: 1, exercicesTermines: 10, totalExercices: 20, challengeTermine: false },
+      { jour: 2, exercicesTermines: 11, totalExercices: 20, challengeTermine: false }
+    ]
+  },
+  {
+    id: 6,
+    nomComplet: "Fatima Script",
+    ville: "Oujda",
+    resultats: [
+      { jour: 1, exercicesTermines: 16, totalExercices: 20, challengeTermine: true }
+    ]
+  },
+  {
+    id: 7,
+    nomComplet: "Mehdi Node",
+    ville: "Nador",
+    resultats: [
+      { jour: 1, exercicesTermines: 2, totalExercices: 20, challengeTermine: false }
+    ]
+  },
+  {
+    id: 8,
+    nomComplet: "Hassan Algo",
+    ville: "Berkane",
+    resultats: [
+      { jour: 1, exercicesTermines: 15, totalExercices: 20, challengeTermine: true }
+    ]
+  },
+  {
+    id: 9,
+    nomComplet: "Siham CSS",
+    ville: "Nador",
+    resultats: [
+      { jour: 1, exercicesTermines: 8, totalExercices: 20, challengeTermine: false }
+    ]
+  },
+  {
+    id: 10,
+    nomComplet: "Bilal React",
+    ville: "Oujda",
+    resultats: [
+      { jour: 1, exercicesTermines: 19, totalExercices: 20, challengeTermine: true }
+    ]
+  },
+  {
+    id: 11,
+    nomComplet: "Khadija HTML",
+    ville: "Al Hoceima",
+    resultats: [
+      { jour: 1, exercicesTermines: 17, totalExercices: 20, challengeTermine: true }
+    ]
+  },
+  {
+    id: 12,
+    nomComplet: "Walid Git",
+    ville: "Berkane",
+    resultats: [
+      { jour: 1, exercicesTermines: 6, totalExercices: 20, challengeTermine: false }
+    ]
+  },
+  {
+    id: 13,
+    nomComplet: "Zineb Frontend",
+    ville: "Nador",
+    resultats: [
+      { jour: 1, exercicesTermines: 13, totalExercices: 20, challengeTermine: false }
+    ]
+  },
+  {
+    id: 14,
+    nomComplet: "Othmane Logic",
+    ville: "Oujda",
+    resultats: [
+      { jour: 1, exercicesTermines: 4, totalExercices: 20, challengeTermine: false }
+    ]
+  },
+  {
+    id: 15,
+    nomComplet: "Houda Fullstack",
+    ville: "Al Hoceima",
+    resultats: [
+      { jour: 1, exercicesTermines: 20, totalExercices: 20, challengeTermine: true }
+    ]
+  },
+  {
+    id: 16,
+    nomComplet: "Ibrahim Backend",
+    ville: "Nador",
+    resultats: [
+      { jour: 1, exercicesTermines: 11, totalExercices: 20, challengeTermine: false }
+    ]
+  },
+  {
+    id: 17,
+    nomComplet: "Nadia Stack",
+    ville: "Berkane",
+    resultats: [
+      { jour: 1, exercicesTermines: 7, totalExercices: 20, challengeTermine: false }
+    ]
+  }
 ];
